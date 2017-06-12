5e2a3df9e10357ee3196fbdfef00e7a4e32ce4fa
luis <luis.castellanos.oviedo@gmail.com>
2017-06-12 18:19:05 -0500

    First commit

---
+++ inventory/src/main/java/com/inventory/entity/Person.java
@@ -0,0 +1,76 @@
+package com.inventory.entity;
+
+import org.springframework.boot.autoconfigure.web.ResourceProperties;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by luis on 6/12/2017.
+ */
+@Entity
+public class Person {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long idPerson;
+    private String name;
+    private String lastName;
+    private String userName;
+    private String passwd;
+
+
+
+    public Person(Long idPerson, String name, String lastName, String userName, String passwd) {
+        this.idPerson = idPerson;
+        this.name = name;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.passwd = passwd;
+    }
+
+
+    public Long getIdPerson() {
+        return idPerson;
+    }
+
+    public void setIdPerson(Long idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+}
